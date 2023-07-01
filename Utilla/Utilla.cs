775f0a7fbe71021618bde2ff5cff801b811c397d
AHauntedArmy <ahauntedarmy@hotmail.co.uk>
2023-07-01 23:25:12 +0100

    fix nullref on button press, dont destroy  plugin  on load.

--- Utilla/Utilla.cs
+++ Utilla/Utilla.cs
@@ -10,13 +10,14 @@ using Photon.Pun;
 namespace Utilla
 {
 
-	[BepInPlugin("org.legoandmars.gorillatag.utilla", "Utilla", "1.6.8")]
+	[BepInPlugin("org.legoandmars.gorillatag.utilla", "Utilla", "1.6.9")]
     public class Utilla : BaseUnityPlugin
     {
         static Events events = new Events();
 
         void Start()
         {
+            DontDestroyOnLoad(this);
             RoomUtils.RoomCode = RoomUtils.RandomString(6); // Generate a random room code in case we need it
 
             GameObject dataObject = new GameObject();
@@ -33,7 +34,9 @@ namespace Utilla
 
         void PostInitialized(object sender, EventArgs e)
 		{
+            // GameObject.DontDestroyOnLoad(this.gameObject);
             var go = new GameObject("CustomGamemodesManager");
+            GameObject.DontDestroyOnLoad(go);
             var gmm = go.AddComponent<GamemodeManager>();
             this.gameObject.GetComponent<UtillaNetworkController>().gameModeManager = gmm;
 		}
